52d3efb444002e757ec46be2e85055c5c3775e9b
fool <1027583934@qq.com>
2021-05-20 20:56:59 +0800

    "login"

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const boom = require('boom')
 var router = express.Router();
+const {
+  CODE_ERROR
+} = require('../utils/constant')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('欢迎来到fool书城')
 });
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
+const Result = require('../models/Result')
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login', function(req, res, next) {
+  console.log(req.body);
+  const {username, password} = req.body
+  if(username === 'admin' && password === 'admin'){
+    new Result('登录成功').success(res)
+  }else{
+    new Result('登录失败').fail(res)
+  }
+  // next()
 });
+
+
 
 module.exports = router;
